d37bee160ee6166ec2ab9e0bd561ddae969e30f8
mikelaurenzo7-collab <mikelaurenzo7@gmail.com>
2026-06-23 05:16:40 -0500

    Include Tidewalk contact runtime coverage

--- tests/run-all.mjs
+++ tests/run-all.mjs
@@ -4,3 +4,4 @@ import "./black-keel-storylets.test.mjs";
 import "./tidewalk-contact.test.mjs";
 import "./tidewalk-contact-field.test.mjs";
 import "./tidewalk-contact-canvas.test.mjs";
+import "./tidewalk-contact-runtime.test.mjs";
