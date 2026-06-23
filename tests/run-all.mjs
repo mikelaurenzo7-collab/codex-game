3e05eea12ab4eb54a5a5acec7b64ac1259ac200e
mikelaurenzo7-collab <mikelaurenzo7@gmail.com>
2026-06-23 04:57:11 -0500

    Include Tidewalk contact canvas coverage

--- tests/run-all.mjs
+++ tests/run-all.mjs
@@ -3,3 +3,4 @@ import "./tidewalk-coastal-objective.test.mjs";
 import "./black-keel-storylets.test.mjs";
 import "./tidewalk-contact.test.mjs";
 import "./tidewalk-contact-field.test.mjs";
+import "./tidewalk-contact-canvas.test.mjs";
