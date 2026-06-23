e2656fb788aa97798760a1cb2dc30a4e9c9be658
mikelaurenzo7-collab <mikelaurenzo7@gmail.com>
2026-06-23 04:19:50 -0500

    Run Tidewalk contact field tests

--- tests/run-all.mjs
+++ tests/run-all.mjs
@@ -2,3 +2,4 @@ import "./game-state.test.mjs";
 import "./tidewalk-coastal-objective.test.mjs";
 import "./black-keel-storylets.test.mjs";
 import "./tidewalk-contact.test.mjs";
+import "./tidewalk-contact-field.test.mjs";
