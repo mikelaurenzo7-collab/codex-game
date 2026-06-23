11aa02c8dffb6b55fe886e1ebba020a22ee82f36
mikelaurenzo7-collab <mikelaurenzo7@gmail.com>
2026-06-23 16:14:26 -0500

    test(brinehook): register command deck bridge suite

--- tests/run-all.mjs
+++ tests/run-all.mjs
@@ -24,3 +24,4 @@ import "./brinehook-client-contract.test.mjs";
 import "./brinehook-encounter.test.mjs";
 import "./brinehook-resolution.test.mjs";
 import "./brinehook-command-brief.test.mjs";
+import "./brinehook-command-deck.test.mjs";
